4ef9cab5805b4eaf7d796a8bb064abac6eac0d9d
shenyanchun <1318444519@qq.com>
2021-05-07 16:01:33 +0800

    Initial commit

---
+++ src/main/java/com/shen/testeverything/TestEverythingApplication.java
@@ -0,0 +1,13 @@
+package com.shen.testeverything;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestEverythingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestEverythingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shen/testeverything/TestEverythingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shen.testeverything;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestEverythingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
